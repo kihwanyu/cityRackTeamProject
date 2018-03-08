de850fc0eb78760281cce5bbb4a469c46b2ce113
user2 <user2@KH_C>
2018-03-08 20:35:09 +0900

    dd

--- cityRack/src/com/kh/cityrack/board/admin/controller/BoardInsertServlet.java
+++ cityRack/src/com/kh/cityrack/board/admin/controller/BoardInsertServlet.java
@@ -27,7 +27,7 @@ public class BoardInsertServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at:dd ").append(request.getContextPath());
 	}
 
 	/**
