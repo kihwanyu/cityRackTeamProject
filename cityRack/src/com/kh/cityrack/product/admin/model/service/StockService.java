a6c3fae2528ac338ddc61e68af9ee000af0bfca5
kihwanyu <dbrlghks94@gmail.com>
2018-03-10 21:28:41 +0900

    상품 재고 입력 완료

--- cityRack/src/com/kh/cityrack/product/admin/model/service/StockService.java
+++ cityRack/src/com/kh/cityrack/product/admin/model/service/StockService.java
@@ -1,5 +1,43 @@
 package com.kh.cityrack.product.admin.model.service;
 
+import java.sql.Connection;
+
+import com.kh.cityrack.product.admin.model.dao.StockDao;
+import com.kh.cityrack.product.admin.model.dto.Stock;
+import static com.kh.cityrack.common.JDBCTemplet.*;
 public class StockService {
+
+	public int StockInsert(Stock s) {
+		Connection conn = getConnection();
+			
+		int result = new StockDao().stockInsert(conn, s);
+		
+		if(result > 0){
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+
+	public int StockTotal(String pcode, Stock s) {
+		Connection conn = getConnection();
+		
+		int result = new StockDao().stockTotal(conn, pcode);
+		
+		if(s.getDivsion().equals("입고")){
+			result += s.getAmount();
+		} else {
+			result -= s.getAmount();
+		}
+		
+		
+		close(conn);
+		
+		return result;
+	}
 
 }
